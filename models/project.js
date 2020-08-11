a994e5b793e18a849694fc88d670e1e73b09dc7b
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-08-11 16:53:24 +0200

    Group find all/find one logic in a model

---
+++ models/project.js
@@ -0,0 +1,57 @@
+const connexion = require('../conf')
+
+const getSelectSql = (onlyOne = false) => {
+  const where = onlyOne ? `WHERE project.id = ?` : ''
+  return `
+    SELECT
+      project.id, title, description, image, url_github, url_test, date,
+      t.name, t.image_name
+    FROM project
+    JOIN project_techno pt ON pt.project_id=project.id
+    JOIN techno t ON pt.techno_id=t.id
+    ${where}
+    GROUP BY project.id, t.id
+    ORDER BY project.date DESC
+  `
+}
+
+const formatResults = (result) => {
+  // create a table with uniq project ids
+  const idProjects = result.reduce((previousValue, item) => {
+    const idExist = previousValue.includes(item.id)
+    return idExist ? previousValue : [...previousValue, item.id]
+  }, [])
+  // Initialize a new empty tab to receive uniq projects
+  // For each project, we create an object which receive the main datas and an array of technologies
+  return idProjects.map((projectId) => {
+    const rows = result.filter((el) => el.id === projectId)
+    const { name, image_name: imageName, ...mainDatas } = rows[0]
+    const currentProject = { mainDatas }
+    const technos = rows.map(({ name, image_name: imageName }) => ({
+      name,
+      image_name: imageName
+    }))
+    currentProject.technos = technos
+    return currentProject
+  })
+}
+
+module.exports = {
+  findAll(cb) {
+    const sql = getSelectSql()
+    connexion.query(sql, (err, result) => {
+      if (err) return cb(err)
+      const projects = formatResults(result)
+      cb(null, projects)
+    })
+  },
+
+  findOneById(projectId, cb) {
+    const sql = getSelectSql(true)
+    connexion.query(sql, [projectId], (err, result) => {
+      if (err) return cb(err)
+      const projects = formatResults(result)
+      cb(null, projects[0])
+    })
+  }
+}
--- routes/projects.js
+++ routes/projects.js
@@ -2,74 +2,47 @@ const express = require('express')
 const router = express.Router()
 
 const connexion = require('../conf')
+const projectModel = require('../models/project')
 
 const isDev = process.env.NODE_ENV === 'production'
 
 // fetch all projects
 router.get('/', (req, res) => {
-  let sql =
-    'SELECT project.id, title, description, image, url_github, url_test, date, '
-  sql += 't.name, t.image_name '
-  sql += 'FROM project '
-  sql += 'JOIN project_techno pt ON pt.project_id=project.id '
-  sql += 'JOIN techno t ON pt.techno_id=t.id '
-  sql += 'GROUP BY project.id, t.id '
-  sql += 'ORDER BY project.date DESC'
-
-  connexion.query(sql, (err, result) => {
+  projectModel.findAll((err, projects) => {
     if (err) {
+      console.error(err)
       return res.status('500').json({
         message: err.message,
         sql: isDev && err.sql
       })
     }
-    // create a table with uniq project ids
-    const idProjects = result.reduce((previousValue, item) => {
-      const idExist = previousValue.includes(item.id)
-      return idExist ? previousValue : [...previousValue, item.id]
-    }, [])
-    // Initialize a new empty tab to receive uniq projects
-    const projects = []
-
-    // For each project, we create an object which receive the main datas and an array of technologies
-    idProjects.map((project, id) => {
-      const currentProject = {}
-      const technos = []
-      result
-        .filter((el) => el.id === project)
-        .map((el) => {
-          const { name, image_name: imageName, ...mainDatas } = el
-          currentProject.mainDatas = mainDatas
-          technos.push({ name, image_name: imageName })
-        })
-      currentProject.technos = technos
-      projects.push(currentProject)
-    })
-    res.status(200).json(projects)
+    res.json(projects)
   })
 })
 
 // fetch a particular projects
 router.get('/:id', (req, res) => {
-  connexion.query(
-    'SELECT * from project WHERE ID = ?',
-    req.params.id,
-    (err, result) => {
-      if (err) {
-        return res.status('500').json({
-          message: err.message,
-          sql: err.sql
-        })
-      }
-      return res.status(200).send(result)
+  projectModel.findOneById(req.params.id, (err, result) => {
+    if (err) {
+      return res.status('500').json({
+        message: err.message,
+        sql: err.sql
+      })
     }
-  )
+    return res.status(200).send(result)
+  })
 })
 
 // Post a new project
 router.post('/', (req, res) => {
   const { project, techno } = req.body
-  const requiredFields = ['title', 'description', 'image', 'url_github', 'url_test']
+  const requiredFields = [
+    'title',
+    'description',
+    'image',
+    'url_github',
+    'url_test'
+  ]
   const missingField = requiredFields.some((field) => !project[field])
   console.log('missingField', missingField)
   if (missingField) {
